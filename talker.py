aa092b1ee866cbd00d761f03c40c09c527484def
tirthjagad <58304820+tirthjagad@users.noreply.github.com>
2020-05-02 01:52:46 +0200

    Update talker.py

--- talker.py
+++ talker.py
@@ -1,24 +1,37 @@
-#! /usr/bin/python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
 import rospy
-from std_msgs.msg import Float32MultiArray
-def talker():
-    pub_p = rospy.Publisher('lefttop_point', Float32MultiArray, queue_size=1)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-	array = [521,1314]
-	left_top = Float32MultiArray(data=array)
-                 # can also be assigned in the following form
-        #left_top = Float32MultiArray()
-        #left_top.data = [521,1314]
-        #left_top.label = 'love'
-        rospy.loginfo(left_top)
-	pub_p.publish(left_top)
-        rate.sleep()
- 
-if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
+from sensor_msgs.msg import LaserScan
+
+rospy.init_node('laser_scan_publisher')
+
+scan_pub = rospy.Publisher('scan', LaserScan, queue_size=50)
+
+num_readings = 100
+laser_frequency = 40
+
+count = 0
+r = rospy.Rate(1.0)
+while not rospy.is_shutdown():
+    current_time = rospy.Time.now()
+
+    scan = LaserScan()
+
+    scan.header.stamp = current_time
+    scan.header.frame_id = 'laser_frame'
+    scan.angle_min = -1.57
+    scan.angle_max = 1.57
+    scan.angle_increment = 3.14 / num_readings
+    scan.time_increment = (1.0 / laser_frequency) / (num_readings)
+    scan.range_min = 0.0
+    scan.range_max = 100.0
+
+    scan.ranges = []
+    scan.intensities = []
+    for i in range(0, num_readings):
+        scan.ranges.append(1.0 * count)  # fake data
+        scan.intensities.append(1)  # fake data
+
+    scan_pub.publish(scan)
+    count += 1
+    r.sleep()
