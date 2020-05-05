8334682c4e0c2370ba244354cce706f362fd13ac
tirthjagad <58304820+tirthjagad@users.noreply.github.com>
2020-05-05 20:10:43 +0200

    Update control.py

--- control.py
+++ control.py
@@ -4,7 +4,7 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Float32
 rospy.init_node('laser_scan_node')
-def callback(msg)
+def callback(msg):
   print msg.ranges[360]
   move.Float32 = 1.0
   if msg.ranges[360] < 2 :
