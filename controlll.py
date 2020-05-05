fba03b4d1a7bbdd80a8b8ba8faf50d1d978a868f
tirthjagad <58304820+tirthjagad@users.noreply.github.com>
2020-05-05 21:01:01 +0200

    Update controlll.py

--- controlll.py
+++ controlll.py
@@ -8,15 +8,14 @@ rospy.init_node('laser_scan_node')
 def callback(msg):
   
   print msg.ranges[360]
-  move.Float32 = 1.0
+  Float32 = 1.0
   if msg.ranges[360] < 2 :
-    move.Flaot32 = 0
+    Flaot32 = 0
   pub.publish(move)
 
 
 sub = rospy.Subscriber('sim_ros_interface/front_scan', LaserScan , callback)
 pub = rospy.Publisher('/Motor1Speed_youbot' , Float32)
-move = Float32()
 rospy.spin()
 
 
